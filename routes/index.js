6c3f2cc00213a4a940da4037df25cdd72b2d4dbc
LucasMcL <LMcLaughlin09@gmail.com>
2017-04-25 09:46:51 -0500

    added dummy data to root rotue

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.use(require('./zookeeper-route'))
 router.use(require('./animal-route'))
 router.get('/',function (req,res) {
   res.json ({
+  	"test": "test",
   	"animals": "https://zoo-api.herokuapp.com/api/v1/animals",
     "zookeepers" : "https://zoo-api.herokuapp.com/api/v1/zookeepers",
   })
