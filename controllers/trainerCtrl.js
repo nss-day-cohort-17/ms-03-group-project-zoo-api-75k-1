393dacb1bdaacc4d9910328b9c95cf5be46e15fd
Jufebrown <jufebrown@gmail.com>
2017-04-26 13:24:50 -0500

    added trainerCtrl

---
+++ controllers/trainerCtrl.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { bookshelf } = require('../db/database');
+const Trainer = require('../models/trainer')
+
+
+module.exports.getAllTrainers = (req,res,next) => {
+  // calls getAllTrainers method on Trainer model
+  Trainer.getAllTrainers()
+  .then( (trainers) => {
+    // returns a 200 status
+    res.status(200).json({trainers});
+  })
+  .catch( (error) => {
+    next(error);
+  });
+}
+
+module.exports.addTrainer = ({body},res,next) =>{
+  Trainer.forge(body)
+  .save()
+  .then(() => res.status(200).json({"msg" : "trainer added successfully"}))
+  .catch((err) => { next(err)})
+}
+
+module.exports.deleteTrainer = ({params: {id}}, res, next) =>{
+  Trainer.deleteTrainer(id)
+  .then(() => res.status(200).json({"msg" : "deleted successfully"}))
+  .catch((err) => { next(err)})
+}
