4e55c2c760b99fbc597a8bf01aead68b042246fa
Jufebrown <jufebrown@gmail.com>
2017-04-26 10:26:17 -0500

    added comments to animal model

--- models/animal.js
+++ models/animal.js
@@ -13,17 +13,23 @@ const Animal = bookshelf.Model.extend({
   // sets relationship with trainers
   trainers: function () { return this.belongsToMany('Trainer').through('AnimalTrainer')}
 }, {
+  // gets all animals with a specified zookeeper
 	getAllWithRelated: function() {
 		return this.forge().orderBy('id', 'ASC').fetchAll({withRelated: ['zookeepers']})
 	},
+  // adds an animal
 	addOne: function(animal) {
 		return this.forge(animal).save({},{require: true})
 	},
+  // deletes and animal
 	delete: function(id) {
 		// Delete animal and all dependents in other tables
 		return this.forge({id}).destroy()
 	},
-	dependents: ['zookeepers'],
+  // use by cascade delete to delete animal records from pivot table
+	dependents: ['zookeepers', 'trainers'],
+
+  // edits animal
   updateAnimal: function(id,{name,age,species}) {
     // console.log("bodyFromModel",{age:10});
     return this.forge({id}).save({name,age,species})
